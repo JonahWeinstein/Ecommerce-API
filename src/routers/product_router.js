d40dc61c88dde198f53d412102573ec17523313d
JonahWeinstein <jonah1weinstein17@gmail.com>
2021-06-17 14:33:08 -0400

    fixed product routes to specify which store is being used

--- src/routers/product_router.js
+++ src/routers/product_router.js
@@ -6,16 +6,16 @@ const Store = require('../models/store')
 const router = new express.Router();
 
 // add a product to a store
-router.post('/products/:store_id', auth, async (req, res) => {
+router.post('/products/add', auth, async (req, res) => {
     try{   
         // see if store exists/does this user have acecss
-        const store = await Store.findOne({ where: { id: req.params.store_id, UserId: req.user.id}})
+        const store = await Store.findOne({ where: { id: req.query.store, UserId: req.user.id}})
         if(!store){
-            return res.status(400).send({error: 'cannot find store'})
+            return res.status(404).send({error: 'cannot find store'})
         }
         const product = Product.build({
             ...req.body,
-            StoreId: req.params.store_id
+            StoreId: req.query.store
         }) 
         await product.save()
         res.status(201).send(product)
@@ -24,18 +24,23 @@ router.post('/products/:store_id', auth, async (req, res) => {
     }
 })
 
-// read all products from a store
-router.get('/products/:store_id', auth, async (req, res) => {
+// read all products from a store specified in the query string
+router.get('/products', auth, async (req, res) => {
     try{
-        const products = await Product.findAll({where: {StoreId: req.params.store_id}})
+        const store = await Store.findOne({ where: { id: req.query.store, UserId: req.user.id}})
+        if(!store){
+            return res.status(400).send({error: 'cannot find store'})
+        }
+        const products = await Product.findAll({where: {StoreId: req.query.store}})
         res.send(products)
 
     } catch(e) {
-        res.status(500).send()
+        res.status(400).send()
     }
 })
-// update a product by id
-router.patch('/products/:id', async (req, res) => {
+// products/update?store=9&product=11
+// update a product by id using the query string
+router.patch('/products/update', auth, async (req, res) => {
      //lets you throw an error when client attempts to update a nonexistent or protected (ex: id) field
      const updates = Object.keys(req.body)
      const allowedUpdates = ['name', 'description', 'quantity', 'price']
@@ -44,16 +49,18 @@ router.patch('/products/:id', async (req, res) => {
          return res.status(400).send({ error: 'Invalid Update'})
      }
     try{
-    const [numberOfAffectedRows, affectedRows] = await Product.update(
-        req.body, 
-        {
-            where: { id: req.params.id}
+        const store = await Store.findOne({ where: { id: req.query.store, UserId: req.user.id}})
+        if(!store){
+            return res.status(400).send({error: 'cannot find store'})
         }
+        const [numberOfAffectedRows, affectedRows] = await Product.update(
+            req.body, 
+            { where: { id: req.query.product, StoreID: req.query.store }}
     )
     if(numberOfAffectedRows == 0){
         return res.status(404).send() 
     }
-    const updatedProduct = await Product.findOne({where: {id:req.params.id}})
+    const updatedProduct = await Product.findOne({where: {id:req.query.product}})
     res.send(updatedProduct)
     } catch (e) {
         res.status(400).send()
